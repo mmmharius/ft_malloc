986f7d49ecafe620d8d44e9b0d305a0a4808ca9c
Marius Papin <mpapin@c2r12p6.42lehavre.fr>
2026-09-01 17:20:53 +0200

    reresize n m, block/zone

--- ft_malloc.c
+++ ft_malloc.c
@@ -23,18 +23,25 @@ t_zone *create_zone(int type, size_t size) {
     else {
         size_t nbr_page = ((size + sizeof(t_zone) + sizeof(t_block)) + page_size - 1) / page_size;
         size_t size_mmap = nbr_page * page_size;
-        printf("nbr_page: %zu, size : %zu\n", nbr_page, size);
+        // printf("nbr_page: %zu, size : %zu\n", nbr_page, size);
         ptr = mmap(NULL, size_mmap, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        ptr = NULL;
         size = size_mmap;
-        printf("after mmap size : %zu\n", size);
+        // printf("after mmap size : %zu\n", size);
     }
     if (ptr == MAP_FAILED)
-        return(perror("mmap:"), NULL);
+        return(NULL);
     t_zone *zone = (t_zone *)ptr;
     zone->size = size;
     zone->type = type;
-    printf("%d | %zu\n", zone->type, zone->size);
-    return zone;
+    t_block *first_b = (t_block *)(zone + 1); // zone + 1 -> zone + 32oct
+    first_b->size = zone->size - sizeof(t_block) - sizeof(t_zone);
+    first_b->free = 1;
+    first_b->prev = NULL;
+    first_b->next = NULL;
+    zone->blocks = first_b;
+    // printf("%d | %zu\n", zone->type, zone->size);
+    return (zone);
 }
 
 int    get_alloc_size(size_t size) {
@@ -48,9 +55,15 @@ int    get_alloc_size(size_t size) {
 }
 
 void    *ft_malloc(size_t size) {
-    t_zone  zone;
     t_block block;
     int type = get_alloc_size(size);
-    create_zone(type, size);
-    printf("type:%d\n", type);
+    t_zone *zone = create_zone(type, size);
+    printf(zone)
+    if (zone == NULL)
+        return (perror("mmap:"), NULL); 
+    return (zone->blocks);
+}
+
+void    ft_free(void *ptr) {
+    munmap(ptr, jsp wala);
 }
