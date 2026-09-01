6f1a8ed4128b3103841aebcf0582f49bb8ba5cde
Marius Papin <mpapin@c2r12p6.42lehavre.fr>
2026-09-01 11:28:25 +0200

    tiny/small resize for add t_zone, mmap by zone

--- ft_malloc.c
+++ ft_malloc.c
@@ -3,10 +3,6 @@
 #include <sys/mman.h> //mmap
 
 t_zone *create_zone(int type, size_t size) {
-
-}
-
-int    get_alloc_size(size_t size) {
     #if OS_TYPE == 1
         long page_size = sysconf(_SC_PAGESIZE);
     #elif OS_TYPE == 2
@@ -15,9 +11,37 @@ int    get_alloc_size(size_t size) {
     # warning "OS_TYPE not know ! use macOs/Linux pls"
         return 0;
     #endif
-    if (size <= 90) // (90 + 32) * 100 = 12200 (88oct lost: 4096x3 = 12288);
+    void *ptr;
+    if (type == 1) {
+        ptr = mmap(NULL, 12288, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        size = 12288;
+    }
+    else if (type == 2) {
+        ptr = mmap(NULL, 69632, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        size = 69632;
+    }
+    else {
+        size_t nbr_page = ((size + sizeof(t_zone) + sizeof(t_block)) + page_size - 1) / page_size;
+        size_t size_mmap = nbr_page * page_size;
+        printf("nbr_page: %zu, size : %zu\n", nbr_page, size);
+        ptr = mmap(NULL, size_mmap, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        size = size_mmap;
+        printf("after mmap size : %zu\n", size);
+    }
+    if (ptr == MAP_FAILED)
+        return(perror("mmap:"), NULL);
+    t_zone *zone = (t_zone *)ptr;
+    zone->size = size;
+    zone->type = type;
+    printf("%d | %zu\n", zone->type, zone->size);
+    return zone;
+}
+
+int    get_alloc_size(size_t size) {
+
+    if (size <= 90) // 32 + ( (90 + 32) * 100 ) = 12232 (56oct lost: 4096x3 = 12288);
         return 1;
-    if (size <= 992) // (992 + 32 ) * 100 = 102400 (4096 * 25 = 102400 : 0 lost)
+    if (size <= 664) // 32 + ( (664 + 32 ) * 100 ) = 69632 (4096 * 17 = 69632 : 0 lost)
         return 2;
     else
         return 3;
@@ -27,6 +51,6 @@ void    *ft_malloc(size_t size) {
     t_zone  zone;
     t_block block;
     int type = get_alloc_size(size);
-    zone = create_zone(type, size);
+    create_zone(type, size);
     printf("type:%d\n", type);
 }
--- main.c
+++ main.c
@@ -3,20 +3,9 @@
 int main() {
     struct t_block;
     struct t_zone;
-    // int lenght = 6;
-    // void *zone = mmap(NULL, lenght, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    // if (zone == MAP_FAILED)
-    //     return(perror("mmap:"), -1);
-    // char hello[6] = "hello\n";
-    // ft_memcpy(zone, hello, 6);
-    // char *str = (char *)zone;
-    // for (int i = 0; str[i]; i++)
-    //     printf("%c", str[i]);
-    // munmap(zone, lenght);
+
     ft_malloc(1);
-    ft_malloc(900);
+    ft_malloc(992);
     ft_malloc(100000);
-    printf("%lu", sizeof(t_block));
-    printf("%lu", sizeof(t_zone));
     return(0);
 }
